4862696dbe8afc2d8d7a2999b71ce2a9fe248a15
satocoder <satokarashi@gmail.com>
2021-01-24 00:58:31 +0330

    User and profile edite processing inserted

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     #path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('edit/',views.edit,name='edit'),
 ]
