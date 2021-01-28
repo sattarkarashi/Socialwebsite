ebf6a7120ba7ca6682985ee49553bd5d968d95d5
satocoder <satokarashi@gmail.com>
2021-01-28 23:08:38 +0330

    Thumbnail insertion

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
-
+from django.shortcuts import get_object_or_404
+from .models import Image
 
 @login_required
 def image_create(request):
@@ -22,5 +23,10 @@ def image_create(request):
     else:
         # build form with data provided by the bookmarklet via GET
         form = ImageCreateForm(data=request.GET)
-        
+
     return render(request,'images/image/create.html',{'section': 'images','form': form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,'images/image/detail.html',{'section': 'images','image': image})
